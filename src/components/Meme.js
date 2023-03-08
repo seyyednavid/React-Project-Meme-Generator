a6e53bf092620b73a1d668c5d0f0e1abd8b606c1
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-03-08 12:06:26 +0000

    Using useState for getting random image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,33 +1,34 @@
-
-import memesData  from "../memesData";
-import {useState} from "react";
-
+import memesData from "../memesData";
+import { useState } from "react";
 
 function Meme() {
- const [memeImage, setMemeImage] = useState('');
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-function getMemeImage() {
-  const memeArray = memesData.data.memes;
-  const randomNumber = Math.floor(Math.random() * memeArray.length);
-  const {url} = memeArray[randomNumber];
-  setMemeImage(url);
-}
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  function getMemeImage() {
+    const memeArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    const url = memeArray[randomNumber].url;
+    setMeme((prevMeme) => ({ ...prevMeme, randomImage: url }));
+  }
 
   return (
     <main>
       <div className="form">
         <input className="form--input" type="text" placeholder="Top text" />
-        <input
-          className="form--input"
-          type="text"
-          placeholder="Bottom text"
-        />
-        <button onClick={getMemeImage} className="form--button">Get a new meme image 🖼</button>
-        <img src={memeImage} className="meme--image" />
+        <input className="form--input" type="text" placeholder="Bottom text" />
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+        <img src={meme.randomImage} className="meme--image" />
       </div>
     </main>
   );
-  
 }
 
 export default Meme;
